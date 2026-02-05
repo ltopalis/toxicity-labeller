c358e64a5d6b675f5685f29075ba42a4756020a4
Lazaros Topalis <83336644+ltopalis@users.noreply.github.com>
2026-02-05 23:10:14 +0200

    extend json limits

--- backend-server/index.js
+++ backend-server/index.js
@@ -10,8 +10,8 @@ const allowedOrigins = [
 ];
 
 const app = express();
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cors({
     origin: function (origin, callback) {
